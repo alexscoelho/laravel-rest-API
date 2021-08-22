ff80837987b94843637065fd4151b16928bc8e23
alexscoelho <alexsoncoelho21@hotmail.com>
2021-08-22 20:59:02 +0000

    added login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $fields['name'],
             'email' => $fields['email'],
-            'password' => bcrypt($fields['name']),
+            'password' => bcrypt($fields['password']),
         ]);
 
         $token = $user->createToken('myapptoken')->plainTextToken;
@@ -30,6 +30,32 @@ class AuthController extends Controller
 
         return response($response,201);
     }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message'=> 'Bad Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response,201);
+    }
+
 
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
